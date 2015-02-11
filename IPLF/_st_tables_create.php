613dfb2aae53b7b4d23138b2cfea53622c25c783
konst-t <konst-t@users.noreply.github.com>
2015-02-11 13:46:02 +0300

    Update _st_tables_create.php

--- IPLF/_st_tables_create.php
+++ IPLF/_st_tables_create.php
@@ -7,7 +7,7 @@ require_once('db.classes.php');
 $database = new DB();
 $database->log_errors = false;
 
-$query="CREATE TABLE IF NOT EXISTS ".DB_PREFIX."components (
+$query="CREATE TABLE IF NOT EXISTS `".DB_PREFIX."components` (
 	`id` int(3) NOT NULL,
 	`component` varchar(255) NOT NULL,
 	`name` varchar(255) NOT NULL,
@@ -22,7 +22,7 @@ $query="CREATE TABLE IF NOT EXISTS ".DB_PREFIX."components (
 
 $database->Go($query);
 
-$query="CREATE TABLE IF NOT EXISTS ".DB_PREFIX."sections (
+$query="CREATE TABLE IF NOT EXISTS `".DB_PREFIX."sections` (
 	`id` int(3) NOT NULL,
 	`section` varchar(255) NOT NULL,
 	`name` varchar(255) NOT NULL,
@@ -34,7 +34,7 @@ $query="CREATE TABLE IF NOT EXISTS ".DB_PREFIX."sections (
 
 $database->Go($query);
 
-$query="CREATE TABLE IF NOT EXISTS ".DB_PREFIX."settings (
+$query="CREATE TABLE IF NOT EXISTS `".DB_PREFIX."settings` (
 	`name` varchar(255) NOT NULL,
 	`value` varchar(255) NOT NULL,
 	INDEX (`name`)
@@ -42,7 +42,7 @@ $query="CREATE TABLE IF NOT EXISTS ".DB_PREFIX."settings (
 
 $database->Go($query);
 
-$query="CREATE TABLE IF NOT EXISTS ".DB_PREFIX."aliases (
+$query="CREATE TABLE IF NOT EXISTS `".DB_PREFIX."aliases` (
 	`alias` varchar(255) NOT NULL,
 	`url` varchar(255) NOT NULL,
 	INDEX (`alias`)
@@ -50,7 +50,7 @@ $query="CREATE TABLE IF NOT EXISTS ".DB_PREFIX."aliases (
 
 $database->Go($query);
 
-$query="CREATE TABLE IF NOT EXISTS ".DB_PREFIX."users (
+$query="CREATE TABLE IF NOT EXISTS `".DB_PREFIX."users` (
 	`id` int(3) NOT NULL,
 	`login` varchar(255) NOT NULL,
 	`name` varchar(255) NOT NULL,
@@ -65,23 +65,23 @@ $query="CREATE TABLE IF NOT EXISTS ".DB_PREFIX."users (
 
 $database->Go($query);
 
-$query="CREATE TABLE ".DB_PREFIX."users_groups (
-	`id` INT( 3 ) NOT NULL ,
-	`name` VARCHAR( 255 ) NOT NULL ,
-	`identifier` VARCHAR( 255 ) NOT NULL ,
-	`description` TEXT NOT NULL,
-	`areas` VARCHAR( 255 ) NOT NULL ,
+$query="CREATE TABLE `".DB_PREFIX."users_groups` (
+	`id` int(3) NOT NULL ,
+	`name` varchar(255) NOT NULL ,
+	`identifier` varchar(255) NOT NULL ,
+	`description` text NOT NULL,
+	`areas` varchar(255) NOT NULL ,
 	`active` tinyint(1) NOT NULL,
 	INDEX (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=".DB_CHARSET;
 
 $database->Go($query);
 
-$query="CREATE TABLE ".DB_PREFIX."users_groups_access (
-	`group` INT( 3 ) NOT NULL ,
-	`area` INT( 3 ) NOT NULL ,
-	`section` VARCHAR( 255 ) NOT NULL ,
-	`access` INT( 1 ) NOT NULL
+$query="CREATE TABLE `".DB_PREFIX."users_groups_access` (
+	`group` int(3) NOT NULL ,
+	`area` int(3) NOT NULL ,
+	`section` varchar(255) NOT NULL ,
+	`access` int(1) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=".DB_CHARSET;
 
 $database->Go($query);
